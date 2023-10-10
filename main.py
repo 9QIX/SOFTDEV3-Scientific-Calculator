7ad307310d62a1242547359bfb72bdaf84c67c82
9QIX <kirkhendrixbulaong@outlook.com>
2023-10-10 19:24:31 +0800

    calc

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import sympy as sp
 
 
-def calculator():
-    print("Python Calculator")
+def scientific_calculator():
+    print("-" * 76)
+    print("Enter a mathematical expression or 'quit' to exit.")
+    print("-" * 76)
+    print(" ______________________________________________________________________")
+    print("|  _______   _______   _______   _______   _______   _______   _______  |")
+    print("| |       | |       | |       | |       | |       | |       | |       | |")
+    print("| |   7   | |   8   | |   9   | |  (+)  | |  (-)  | |  (*)  | |  (/)  | |")
+    print("| |_______| |_______| |_______| |_______| |_______| |_______| |_______| |")
+    print("| |       | |       | |       | |       | |       | |       | |       | |")
+    print("| |   4   | |   5   | |   6   | | (sin) | | (cos) | | (tan) | | (log) | |")
+    print("| |_______| |_______| |_______| |_______| |_______| |_______| |_______| |")
+    print("| |       | |       | |       | |       | |       | |       | |       | |")
+    print("| |   1   | |   2   | |   3   | | (asin)| | (acos)| | (atan)| | (exp) | |")
+    print("| |_______| |_______| |_______| |_______| |_______| |_______| |_______| |")
+    print("| |       | |       | |       | |       | |       | |       | |       | |")
+    print("| |   0   | |   .   | |   =   | | (sqrt)| |  (^)  | |  (ln) | |(log10)| |")
+    print("| |_______| |_______| |_______| |_______| |_______| |_______| |_______| |")
+    print("|_______________________________________________________________________|")
 
     while True:
-        print("-" * 50)
+        print("-" * 76)
         print("Enter a mathematical expression or 'quit' to exit.")
-        print("-" * 50)
+        print("-" * 76)
 
         expression = input(">>> ")
 
@@ -21,12 +38,12 @@ def calculator():
                 # If the result is a symbolic expression, evaluate it numerically
                 result = float(result)
 
-            print("-" * 50)
+            print("-" * 76)
             print("Result:", result)
         except (ValueError, sp.SympifyError):
-            print("-" * 50)
+            print("-" * 76)
             print("Invalid input. Please enter a valid expression.")
 
 
 if __name__ == "__main__":
-    calculator()
+    scientific_calculator()
